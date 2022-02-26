468dfa4ff780f84e0cd9ff3c05b33a6a04c13318
maskishe <kurzin.mikhail@gmail.com>
2022-02-27 00:40:12 +0300

    Welcome route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello/{name}', fn(string $name) => 'Hello, {$name}');
+
+Route::get('/welcome', function () {
+    return 'Welcome';
+});
